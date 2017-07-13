c60e9585ab020fe29cf049951e0289344b1e7ece
Jared Sorge <jared@jsorge.net>
2017-07-13 10:12:25 -0700

    Initial commit, added ignore

---
+++ TPHCloudAnalytics/TPHCloudAnalytics/TPHCloudAnalytics.h
@@ -0,0 +1,19 @@
+//
+//  TPHCloudAnalytics.h
+//  TPHCloudAnalytics
+//
+//  Created by Jared Sorge on 7/13/17.
+//  Copyright © 2017 Taphouse Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPHCloudAnalytics.
+FOUNDATION_EXPORT double TPHCloudAnalyticsVersionNumber;
+
+//! Project version string for TPHCloudAnalytics.
+FOUNDATION_EXPORT const unsigned char TPHCloudAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPHCloudAnalytics/PublicHeader.h>
+
+
